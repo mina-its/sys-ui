04eade06d149f69464453dad098dd3516c353fe1
git <farhad.jalali@gmail.com>
2020-04-18 19:23:07 +0300

    remove system config

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as types from './src/types';
-import * as main from './src/main';
-// @ts-ignore
-import FormView from "./src/components/FormView.vue";
-// @ts-ignore
-export default {FormView, ...main, ...types};
-
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import * as types from './types';
 import * as main from './main';
 import FormView from "./components/FormView.vue";
 
-export default {FormView, Test, ...main, ...types};
+export default {FormView, ...main, ...types};
